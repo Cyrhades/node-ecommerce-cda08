9fcf6ef5fa3dccc7c222a454ff03f31af66310dc
Cyrhades <cyrhades76@gmail.com>
2021-10-14 15:53:08 +0200

    Ajout des erreurs HTTP 404 et 405

--- app/routes.js
+++ app/routes.js
@@ -1,50 +1,69 @@
+let errorsHTTP = require('./errorsHTTP.js')();
 module.exports = (app) => {
-    app.get('/', (req, res) => {
-        let Controller = require('../src/controllers/Home.js');
-        (new Controller).index(req, res);
-    });
+    
+    app.route('/')
+        .get((req, res) => {
+            let Controller = require('../src/controllers/Home.js');
+            (new Controller).index(req, res);
+        })
+        .all(errorsHTTP.error405);
 
-    app.get('/inscription', (req, res) => {
-        let Controller = require('../src/controllers/Register.js');
-        (new Controller).index(req, res);
-    });
-    app.post('/inscription', (req, res) => {
-        let Controller = require('../src/controllers/Register.js');
-        (new Controller).form(req, res);
-    });
+    app.route('/inscription')
+        .get((req, res) => {
+            let Controller = require('../src/controllers/Register.js');
+            (new Controller).index(req, res);
+        })
+        .post((req, res) => {
+            let Controller = require('../src/controllers/Register.js');
+            (new Controller).form(req, res);
+        })
+        .all(errorsHTTP.error405);
 
 
-    app.get('/connexion', (req, res) => {
+    app.route('/connexion')
+        .get((req, res) => {
+            let Controller = require('../src/controllers/Authenticated.js');
+            (new Controller).index(req, res);
+        })
+        .post((req, res) => {
         let Controller = require('../src/controllers/Authenticated.js');
-        (new Controller).index(req, res);
-    });
-    app.post('/connexion', (req, res) => {
-        let Controller = require('../src/controllers/Authenticated.js');
         (new Controller).form(req, res);
-    });
+        })
+        .all(errorsHTTP.error405);
 
-    app.get('/deconnexion', (req, res) => {
-        let Controller = require('../src/controllers/Authenticated.js');
-        (new Controller).disconnect(req, res);
-    });
+    app.route('/deconnexion')
+        .get((req, res) => {
+            let Controller = require('../src/controllers/Authenticated.js');
+            (new Controller).disconnect(req, res);
+        })
+        .all(errorsHTTP.error405);
 
 
-    app.get('/admin', (req, res) => {
-        let Controller = require('../src/controllers/Admin.js');
-        (new Controller).index(req, res);
-    });
+    app.route('/admin')
+        .get((req, res) => {
+            let Controller = require('../src/controllers/Admin.js');
+            (new Controller).index(req, res);
+        })
+        .all(errorsHTTP.error405);
 
-    app.get('/admin/product', (req, res) => {
-        let Controller = require('../src/controllers/AdminProduct.js');
-        (new Controller).index(req, res);
-    });
+    app.route('/admin/product')
+        .get((req, res) => {
+            let Controller = require('../src/controllers/AdminProduct.js');
+            (new Controller).index(req, res);
+        })
+        .all(errorsHTTP.error405);
 
-    app.get('/admin/product/add', (req, res) => {
-        let Controller = require('../src/controllers/AdminProduct.js');
-        (new Controller).add(req, res);
-    });
-    app.post('/admin/product/add', (req, res) => {
-        let Controller = require('../src/controllers/AdminProduct.js');
-        (new Controller).form(req, res);
-    });
+
+    app.route('/admin/product/add')
+        .get((req, res) => {
+            let Controller = require('../src/controllers/AdminProduct.js');
+            (new Controller).add(req, res);
+        })
+        .post((req, res) => {
+            let Controller = require('../src/controllers/AdminProduct.js');
+            (new Controller).form(req, res);
+        })
+        .all(errorsHTTP.error405);
+
+    app.route("*").all(errorsHTTP.error404);
 };
